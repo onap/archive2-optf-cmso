c36ccc452171c77c6985d0a511b12e1c9fb1b7dd
Jerry Flood <jflood@att.com>
2018-10-17 10:31:22 -0400

    Updates to build docker images
    Issue-ID: OPTFRA-373
    Change-Id: I3461fb4df7526412525c1ab9ca318316a53e27e9
    Signed-off-by: Jerry Flood <jflood@att.com>

---
+++ cmso-database/src/main/docker/extra-files/startService.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "VM_ARGS="${VM_ARGS}
+
+java -Djava.security.egd=file:/dev/./urandom  ${VM_ARGS} -Xms256m -Xmx1024m  -jar  ./app.jar  --spring.config.location=/share/etc/config/liquibase.properties
