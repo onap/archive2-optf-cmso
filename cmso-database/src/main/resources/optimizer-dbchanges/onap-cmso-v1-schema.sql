c0f2c43b2074a3ef76b40c45803ce788ec79dce6
Jerry Flood <jflood@att.com>
2019-03-15 01:08:05 -0400

    DB Schema update for Dublin
    - Added optimizer schema
    - updated cmso schema to use uuid PK
    
    Issue-ID: OPTFRA-459
    Change-Id: I1457b7d06b371298443a451af26e756d7b42f66c
    Signed-off-by: Jerry Flood <jflood@att.com>

---
+++ cmso-database/src/main/resources/optimizer-dbchanges/onap-cmso-v1-schema.sql
@@ -0,0 +1,51 @@
+CREATE SCHEMA IF NOT EXISTS `optimizer` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE IF NOT EXISTS `optimizer`.`request` (
+  uuid BINARY(16) NOT NULL,
+  request LONGTEXT NOT NULL,
+  created_time BIGINT(20) NULL DEFAULT NULL,
+  request_start BIGINT(20) NULL DEFAULT NULL,
+  request_end BIGINT(20) NULL DEFAULT NULL,
+  status VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`uuid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `optimizer`.`response` (
+  uuid BINARY(16) NOT NULL,
+  repsonse LONGTEXT NULL DEFAULT NULL,
+  delivered_time BIGINT(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`uuid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+CREATE TABLE IF NOT EXISTS `optimizer`.`topology` (
+  uuid BINARY(16) NOT NULL,
+  topology LONGTEXT NULL DEFAULT NULL,
+  topology_start BIGINT(20) NULL DEFAULT NULL,
+  topology_end BIGINT(20) NULL DEFAULT NULL,
+  topology_retries INT NULL DEFAULT NULL,
+  PRIMARY KEY (`uuid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `optimizer`.`tickets` (
+  uuid BINARY(16) NOT NULL,
+  tickets LONGTEXT NULL DEFAULT NULL,
+  tickets_start BIGINT(20) NULL DEFAULT NULL,
+  tickets_end BIGINT(20) NULL DEFAULT NULL,
+  tickets_retries INT NULL DEFAULT NULL,
+  PRIMARY KEY (`uuid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `optimizer`.`optimizer` (
+  uuid BINARY(16) NOT NULL,
+  optimize_response LONGTEXT NULL DEFAULT NULL,
+  optimize_start BIGINT(20) NULL DEFAULT NULL,
+  optimize_end BIGINT(20) NULL DEFAULT NULL,
+  optimize_retries INT NULL DEFAULT NULL,
+  PRIMARY KEY (`uuid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
--- cmso-robot/robot/assets/sql/selectall.sql
+++ cmso-robot/robot/assets/sql/selectall.sql
@@ -1,9 +1,10 @@
-select * from DOMAINS;
-select * from DOMAIN_DATA order by schedules_id desc;
-select * from CHANGE_MANAGEMENT_SCHEDULES order by id desc;
-select * from CHANGE_MANAGEMENT_CHANGE_WINDOWS;
-select * from CHANGE_MANAGEMENT_GROUPS;
-select * from SCHEDULES order by id desc;
-select * from APPROVAL_TYPES;
-select * from SCHEDULE_APPROVALS;
-select * from SCHEDULE_EVENTS;
+select DOMAINS.* from CMSO.DOMAINS;
+select hex(uuid), hex(schedules_uuid), DOMAIN_DATA.* from CMSO.DOMAIN_DATA order by schedules_uuid;
+select hex(uuid), hex(change_management_group_uuid), CHANGE_MANAGEMENT_SCHEDULES.* from CMSO.CHANGE_MANAGEMENT_SCHEDULES order by uuid desc;
+select hex(uuid), hex(schedules_uuid), hex(change_management_group_uuid), CHANGE_MANAGEMENT_CHANGE_WINDOWS.* from CMSO.CHANGE_MANAGEMENT_CHANGE_WINDOWS;
+select hex(uuid), hex(schedules_uuid), CHANGE_MANAGEMENT_GROUPS.* from CMSO.CHANGE_MANAGEMENT_GROUPS;
+select hex(uuid), SCHEDULES.* from CMSO.SCHEDULES order by create_date_time desc;
+select hex(uuid), APPROVAL_TYPES.* from CMSO.APPROVAL_TYPES;
+select hex(uuid), hex(approval_types_uuid), hex(schedules_uuid), SCHEDULE_APPROVALS.* from CMSO.SCHEDULE_APPROVALS;
+select hex(uuid), hex(change_management_schedules_uuid), ELEMENT_DATA.* from CMSO.ELEMENT_DATA;
+
