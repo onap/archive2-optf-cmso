7cff5300430d7a52c202be64aee0e871ef5ba782
Jerry Flood <jflood@att.com>
2019-03-15 14:37:03 -0400

    Initial commit of Optimzer API
    Issue-ID: OPTFRA-437
    Change-Id: Idb509301472b3cc29a9e42c5a4f89aea3aa6190d
    Signed-off-by: Jerry Flood <jflood@att.com>

---
+++ cmso-optimizer/src/main/java/org/onap/observations/Observation.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright © 2019 AT&T Intellectual Property.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         https://creativecommons.org/licenses/by/4.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+package org.onap.observations;
+
+import org.apache.log4j.Level;
+
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
+
+
+public class Observation 
+{
+	private static EELFLogger log = EELFManager.getInstance().getLogger(Observation.class);
+	private static EELFLogger metrics = EELFManager.getInstance().getMetricsLogger();
+	private static EELFLogger audit = EELFManager.getInstance().getAuditLogger();
+	private static EELFLogger errors = EELFManager.getInstance().getErrorLogger();
+	private static EELFLogger debug = EELFManager.getInstance().getDebugLogger();
+	
+	//*************************************************************************************************
+	public static void report(ObservationInterface o, Exception e, String ...arguments) 
+	{
+		Mdc.setCaller(4);
+		Mdc.setObservation(o);
+		if (o.getAudit())
+		{
+			audit.info(o, e, arguments);
+		}
+		if (o.getMetric())
+		{
+			metrics.info(o, e, arguments);
+		}
+		Level l = o.getLevel();
+		switch (l.toInt())
+		{
+		case Level.WARN_INT:
+			errors.warn(o, arguments);
+			debug.debug(o, e, arguments);
+			break;
+		case Level.INFO_INT:
+			log.info(o, e, arguments);
+			debug.debug(o, e, arguments);
+			break;
+		case Level.ERROR_INT:
+			errors.error(o, arguments);
+			debug.debug(o, e, arguments);
+			break;
+		case Level.TRACE_INT:
+			debug.trace(o, e, arguments);
+			break;
+		case Level.DEBUG_INT:
+			debug.debug(o, e, arguments);
+			break;
+		default:
+			log.info(o, e, arguments);
+		}
+		Mdc.clearCaller();
+	}
+	
+	public static void report(ObservationInterface o, String ...arguments) 
+	{
+		Mdc.setCaller(4);
+		Mdc.setObservation(o);
+		if (o.getAudit())
+		{
+			audit.info(o, arguments);
+		}
+		if (o.getMetric())
+		{
+			metrics.info(o, arguments);
+		}
+		Level l = o.getLevel();
+		switch (l.toInt())
+		{
+		case Level.WARN_INT:
+			errors.warn(o, arguments);
+			debug.debug(o, arguments);
+			break;
+		case Level.INFO_INT:
+			log.info(o, arguments);
+			debug.debug(o, arguments);
+			break;
+		case Level.ERROR_INT:
+			errors.error(o, arguments);
+			debug.debug(o, arguments);
+			break;
+		case Level.TRACE_INT:
+			debug.debug(o, arguments);
+			break;
+		case Level.DEBUG_INT:
+			debug.debug(o, arguments);
+			break;
+		default:
+			log.info(o, arguments);
+		}
+		Mdc.clearCaller();
+	}
+
+}
---
+++ cmso-optimizer/src/main/java/org/onap/optf/cmso/optimizer/service/rs/models/HealthCheckMessage.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright © 2017-2018 AT&T Intellectual Property.
+ * Modifications Copyright © 2018 IBM.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         https://creativecommons.org/licenses/by/4.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.onap.optf.cmso.optimizer.service.rs.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.ApiModel;
+
+@ApiModel
+public class HealthCheckMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static EELFLogger log = EELFManager.getInstance().getLogger(HealthCheckMessage.class);
+
+    private Boolean healthy = false;
+    private String buildInfo = "";
+    private String currentTime = "";
+    private String hostname = "";
+
+    private List<HealthCheckComponent> components = new ArrayList<HealthCheckComponent>();
+
+    public Boolean getHealthy() {
+        return healthy;
+    }
+
+    public void setHealthy(Boolean healthy) {
+        this.healthy = healthy;
+    }
+
+    public String getBuildInfo() {
+        return buildInfo;
+    }
+
+    public void setBuildInfo(String buildInfo) {
+        this.buildInfo = buildInfo;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public List<HealthCheckComponent> getComponents() {
+        return components;
+    }
+
+    public void setComponents(List<HealthCheckComponent> components) {
+        this.components = components;
+    }
+
+    public void addComponent(HealthCheckComponent components) {
+        this.components.add(components);
+    }
+
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            log.debug("Error in toString()", e);
+        }
+        return "";
+    }
+}
