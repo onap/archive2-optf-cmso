8d0f8d9e255aad41a199f0962eef7ba4cb01fae9
Jerry Flood <jflood@att.com>
2019-03-15 14:37:03 -0400

    Initial commit of Optimzer API
    Issue-ID: OPTFRA-437
    Change-Id: Idb509301472b3cc29a9e42c5a4f89aea3aa6190d
    Signed-off-by: Jerry Flood <jflood@att.com>

---
+++ cmso-optimizer/src/main/java/org/onap/optf/cmso/optimizer/service/rs/models/ElementLocation.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright � 2017-2018 AT&T Intellectual Property.
+ * Modifications Copyright � 2018 IBM.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         https://creativecommons.org/licenses/by/4.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.onap.optf.cmso.optimizer.service.rs.models;
+
+import java.io.Serializable;
+
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "Element Location",
+        description = "Location information necessary to determine timezone."
+        		+ " lat/lon and/or timezone must be provided")
+public class ElementLocation implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static EELFLogger log = EELFManager.getInstance().getLogger(ElementLocation.class);
+
+    @ApiModelProperty(value = "Geographic latitude of element.")
+    private Float lat;
+
+    @ApiModelProperty(value = "Geographic longitude of element.")
+    private Float lon;
+
+    @ApiModelProperty(value = "Timezone.")
+    private String timezone;
+
+    public Float getLat() {
+		return lat;
+	}
+
+	public void setLat(Float lat) {
+		this.lat = lat;
+	}
+
+	public Float getLon() {
+		return lon;
+	}
+
+	public void setLon(Float lon) {
+		this.lon = lon;
+	}
+
+	public String getTimezone() {
+		return timezone;
+	}
+
+	public void setTimezone(String timezone) {
+		this.timezone = timezone;
+	}
+
+	public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            log.debug("Error in toString()", e);
+        }
+        return "";
+    }
+
+}
---
+++ cmso-optimizer/src/test/java/org/onap/optf/cmso/AuthProviderTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2019 IBM Intellectual Property.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.onap.optf.cmso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.onap.optf.cmso.optimizer.AuthProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuthProviderTest {
+
+    @Test
+    public void authenticate() {
+        String principal = "testName";
+        String credential = "testPassword";
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getName()).thenReturn(principal);
+        when(authentication.getCredentials()).thenReturn(credential);
+        AuthProvider authProvider = new AuthProvider();
+        Authentication auth = authProvider.authenticate(authentication);
+        assertEquals(principal, auth.getPrincipal());
+        assertEquals(credential, auth.getCredentials());
+    }
+
+    @Test
+    public void supports() {
+        AuthProvider authProvider = new AuthProvider();
+        assertTrue(authProvider.supports(UsernamePasswordAuthenticationToken.class));
+        assertFalse(authProvider.supports(Authentication.class));
+    }
+}
