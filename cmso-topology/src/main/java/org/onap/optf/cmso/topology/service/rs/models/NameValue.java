212aa9ee3fcce580736d9d0945fe28b6e6c43953
Jerry Flood <jflood@att.com>
2019-03-15 14:49:07 -0400

    Initial commit of Topology API
    Issue-ID: OPTFRA-430
    
    Change-Id: I1e83bafbf7eba2269d4aa00b5c60ea61f49689b3
    Signed-off-by: Jerry Flood <jflood@att.com>

---
+++ cmso-topology/src/main/java/org/onap/optf/cmso/topology/service/rs/models/NameValue.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright © 2017-2019 AT&T Intellectual Property.
+ * Modifications Copyright © 2018 IBM.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * 
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         https://creativecommons.org/licenses/by/4.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.onap.optf.cmso.topology.service.rs.models;
+
+import java.io.Serializable;
+
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "Name Value Data",
+        description = "Instance of a name/value")
+public class NameValue implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static EELFLogger log = EELFManager.getInstance().getLogger(NameValue.class);
+
+    public NameValue()
+    {
+    	
+    }
+
+    public NameValue(String name, Object value)
+    {
+    	this.name = name;
+    	this.value = value;
+    }
+    
+    @ApiModelProperty(value = "Name.")
+    private String name;
+
+    @ApiModelProperty(value = "Value.")
+    private Object value;
+    
+    
+    public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Object getValue() {
+		return value;
+	}
+
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+
+	public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            log.debug("Error in toString()", e);
+        }
+        return "";
+    }
+
+}
