c94045026c7bb0ddf493f4dafccc8a60465f9928
Jerry Flood <jflood@att.com>
2019-03-20 16:26:09 -0400

    Update robot docker to python3
    Fix style stuff
    
    Issue-ID: OPTFRA-462
    Change-Id: Idc039fddd1528bf083d5cf9158f6636d2dc3887a
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/test/java/org/onap/optf/cmso/utilities/PropertiesAdmin.java
+++ cmso-service/src/test/java/org/onap/optf/cmso/utilities/PropertiesAdmin.java
@@ -1,52 +1,56 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
+ * Copyright Â© 2017-2018 AT&T Intellectual Property.
+ * Modifications Copyright Â© 2018 IBM.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
+ *
+ *
  * Unless otherwise specified, all documentation contained herein is licensed
  * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
  * you may not use this documentation except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         https://creativecommons.org/licenses/by/4.0/
- * 
+ *
  * Unless required by applicable law or agreed to in writing, documentation
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 package org.onap.optf.cmso.utilities;
 
 import org.onap.optf.cmso.common.PropertiesManagement;
 
 public class PropertiesAdmin {
-    public static void main(String[] args) {
-        PropertiesManagement pm = new PropertiesManagement();
-        if (args.length < 1) {
-            System.out.println("Missing argument");
-            return;
-        }
-        String value = "";
-        if (args[0].startsWith("dec:")) {
-            value = PropertiesManagement.getDecryptedValue(args[0].substring(4));
-        } else {
-            value = pm.getEncryptedValue(args[0]);
-        }
-        System.out.println(args[0] + " : " + value);
+  /**
+   * Test properties admin.
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    PropertiesManagement pm = new PropertiesManagement();
+    if (args.length < 1) {
+      System.out.println("Missing argument");
+      return;
     }
-
+    String value = "";
+    if (args[0].startsWith("dec:")) {
+      value = PropertiesManagement.getDecryptedValue(args[0].substring(4));
+    } else {
+      value = pm.getEncryptedValue(args[0]);
+    }
+    System.out.println(args[0] + " : " + value);
+  }
 }
