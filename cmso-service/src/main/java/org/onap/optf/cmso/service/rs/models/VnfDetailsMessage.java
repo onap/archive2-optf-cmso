d6b8da91b32b8b8ffd2c028412a33247a6ca824a
Jerry Flood <jflood@att.com>
2019-03-25 12:27:19 -0400

    t reCommit 14 for  Create Optimized Sched API
    Multiple commits required due to commit size limitation.
    
    Change-Id: Iad72f1a8f79d6e7949d6f983106340e02e61706d
    Issue-ID: OPTFRA-458
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/main/java/org/onap/optf/cmso/service/rs/models/VnfDetailsMessage.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/service/rs/models/VnfDetailsMessage.java
@@ -1,27 +1,27 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
+ * Copyright Â© 2017-2018 AT&T Intellectual Property.
+ * Modifications Copyright Â© 2018 IBM.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
+ *
+ *
  * Unless otherwise specified, all documentation contained herein is licensed
  * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
  * you may not use this documentation except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         https://creativecommons.org/licenses/by/4.0/
- * 
+ *
  * Unless required by applicable law or agreed to in writing, documentation
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,15 +31,18 @@
 
 package org.onap.optf.cmso.service.rs.models;
 
-import java.io.Serializable;
-import java.util.List;
 import com.att.eelf.configuration.EELFLogger;
 import com.att.eelf.configuration.EELFManager;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.List;
 
+/**
+ * The Class VnfDetailsMessage.
+ */
 @ApiModel(value = "VNF Details", description = "Details and scheduling criteria for the VNFs to be changed.")
 public class VnfDetailsMessage implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -52,33 +55,70 @@ public class VnfDetailsMessage implements Serializable {
     private List<String> node;
 
     @ApiModelProperty(
-            value = "Lists of desired change windows that the optimizer can select from. (Only 1 change window supported at this time)")
+            value = "Lists of desired change windows that the optimize"
+                            + "r can select from. (Only 1 change window supported at this time)")
     private List<ChangeWindowMessage> changeWindow;
 
+    /**
+     * Gets the group id.
+     *
+     * @return the group id
+     */
     public String getGroupId() {
         return groupId;
     }
 
+    /**
+     * Sets the group id.
+     *
+     * @param groupId the new group id
+     */
     public void setGroupId(String groupId) {
         this.groupId = groupId;
     }
 
+    /**
+     * Gets the node.
+     *
+     * @return the node
+     */
     public List<String> getNode() {
         return node;
     }
 
+    /**
+     * Sets the node.
+     *
+     * @param node the new node
+     */
     public void setNode(List<String> node) {
         this.node = node;
     }
 
+    /**
+     * Gets the change window.
+     *
+     * @return the change window
+     */
     public List<ChangeWindowMessage> getChangeWindow() {
         return changeWindow;
     }
 
+    /**
+     * Sets the change window.
+     *
+     * @param changeWindow the new change window
+     */
     public void setChangeWindow(List<ChangeWindowMessage> changeWindow) {
         this.changeWindow = changeWindow;
     }
 
+    /**
+     * To string.
+     *
+     * @return the string
+     */
+    @Override
     public String toString() {
         ObjectMapper mapper = new ObjectMapper();
         try {
