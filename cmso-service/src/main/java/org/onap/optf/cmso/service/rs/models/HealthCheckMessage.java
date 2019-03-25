6d8f5f7b8164dfc22f751fc411e2d0931688ef35
Jerry Flood <jflood@att.com>
2019-03-25 12:27:19 -0400

    Commit 14 for  Create Optimized Sched API
    Multiple commits required due to commit size limitation.
    
    Change-Id: Iad72f1a8f79d6e7949d6f983106340e02e61706d
    Issue-ID: OPTFRA-458
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/main/java/org/onap/optf/cmso/service/rs/models/HealthCheckMessage.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/service/rs/models/HealthCheckMessage.java
@@ -1,27 +1,27 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
+ * Copyright ï¿½ 2017-2018 AT&T Intellectual Property.
+ * Modifications Copyright ï¿½ 2018 IBM.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
+ *
+ *
  * Unless otherwise specified, all documentation contained herein is licensed
  * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
  * you may not use this documentation except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         https://creativecommons.org/licenses/by/4.0/
- * 
+ *
  * Unless required by applicable law or agreed to in writing, documentation
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,15 +31,18 @@
 
 package org.onap.optf.cmso.service.rs.models;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import com.att.eelf.configuration.EELFLogger;
 import com.att.eelf.configuration.EELFManager;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiModel;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * The Class HealthCheckMessage.
+ */
 @ApiModel
 public class HealthCheckMessage implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -52,50 +55,111 @@ public class HealthCheckMessage implements Serializable {
 
     private List<HealthCheckComponent> components = new ArrayList<HealthCheckComponent>();
 
+    /**
+     * Gets the healthy.
+     *
+     * @return the healthy
+     */
     public Boolean getHealthy() {
         return healthy;
     }
 
+    /**
+     * Sets the healthy.
+     *
+     * @param healthy the new healthy
+     */
     public void setHealthy(Boolean healthy) {
         this.healthy = healthy;
     }
 
+    /**
+     * Gets the builds the info.
+     *
+     * @return the builds the info
+     */
     public String getBuildInfo() {
         return buildInfo;
     }
 
+    /**
+     * Sets the builds the info.
+     *
+     * @param buildInfo the new builds the info
+     */
     public void setBuildInfo(String buildInfo) {
         this.buildInfo = buildInfo;
     }
 
+    /**
+     * Gets the current time.
+     *
+     * @return the current time
+     */
     public String getCurrentTime() {
         return currentTime;
     }
 
+    /**
+     * Sets the current time.
+     *
+     * @param currentTime the new current time
+     */
     public void setCurrentTime(String currentTime) {
         this.currentTime = currentTime;
     }
 
+    /**
+     * Gets the hostname.
+     *
+     * @return the hostname
+     */
     public String getHostname() {
         return hostname;
     }
 
+    /**
+     * Sets the hostname.
+     *
+     * @param hostname the new hostname
+     */
     public void setHostname(String hostname) {
         this.hostname = hostname;
     }
 
+    /**
+     * Gets the components.
+     *
+     * @return the components
+     */
     public List<HealthCheckComponent> getComponents() {
         return components;
     }
 
+    /**
+     * Sets the components.
+     *
+     * @param components the new components
+     */
     public void setComponents(List<HealthCheckComponent> components) {
         this.components = components;
     }
 
+    /**
+     * Adds the component.
+     *
+     * @param components the components
+     */
     public void addComponent(HealthCheckComponent components) {
         this.components.add(components);
     }
 
+    /**
+     * To string.
+     *
+     * @return the string
+     */
+    @Override
     public String toString() {
         ObjectMapper mapper = new ObjectMapper();
         try {
--- cmso-service/src/main/java/org/onap/optf/cmso/service/rs/models/ScheduleMessage.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/service/rs/models/ScheduleMessage.java
@@ -1,27 +1,27 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
+ * Copyright ï¿½ 2017-2018 AT&T Intellectual Property.
+ * Modifications Copyright ï¿½ 2018 IBM.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
+ *
+ *
  * Unless otherwise specified, all documentation contained herein is licensed
  * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
  * you may not use this documentation except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         https://creativecommons.org/licenses/by/4.0/
- * 
+ *
  * Unless required by applicable law or agreed to in writing, documentation
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,27 +31,37 @@
 
 package org.onap.optf.cmso.service.rs.models;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.Map;
 import com.att.eelf.configuration.EELFLogger;
 import com.att.eelf.configuration.EELFManager;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
 /**
  * The persistent class for the approval_types database table.
- * 
+ *
  */
 @ApiModel(value = "Schedule Request", description = "Request to schedule VNF change management workflow(s).")
 public abstract class ScheduleMessage implements Serializable {
     private static final long serialVersionUID = 1L;
     private static EELFLogger log = EELFManager.getInstance().getLogger(ScheduleMessage.class);
 
+    /**
+     * Gets the scheduling info.
+     *
+     * @return the scheduling info
+     */
     public abstract Object getSchedulingInfo();
 
+    /**
+     * Sets the scheduling info.
+     *
+     * @param info the new scheduling info
+     */
     public abstract void setSchedulingInfo(Object info);
 
     // public abstract void setSchedulingInfo(Object schedulingInfo);
@@ -71,46 +81,102 @@ public abstract class ScheduleMessage implements Serializable {
     @ApiModelProperty(value = "Domain data as name value/pairs. (i.e. CallbackUrl, CallbackData, WorkflowName)")
     private List<Map<String, String>> domainData;
 
+    /**
+     * Gets the domain.
+     *
+     * @return the domain
+     */
     public String getDomain() {
         return domain;
     }
 
+    /**
+     * Sets the domain.
+     *
+     * @param domain the new domain
+     */
     public void setDomain(String domain) {
         this.domain = domain;
     }
 
+    /**
+     * Gets the schedule id.
+     *
+     * @return the schedule id
+     */
     public String getScheduleId() {
         return scheduleId;
     }
 
+    /**
+     * Sets the schedule id.
+     *
+     * @param scheduleId the new schedule id
+     */
     public void setScheduleId(String scheduleId) {
         this.scheduleId = scheduleId;
     }
 
+    /**
+     * Gets the schedule name.
+     *
+     * @return the schedule name
+     */
     public String getScheduleName() {
         return scheduleName;
     }
 
+    /**
+     * Sets the schedule name.
+     *
+     * @param scheduleName the new schedule name
+     */
     public void setScheduleName(String scheduleName) {
         this.scheduleName = scheduleName;
     }
 
+    /**
+     * Gets the user id.
+     *
+     * @return the user id
+     */
     public String getUserId() {
         return userId;
     }
 
+    /**
+     * Sets the user id.
+     *
+     * @param userId the new user id
+     */
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
+    /**
+     * Gets the domain data.
+     *
+     * @return the domain data
+     */
     public List<Map<String, String>> getDomainData() {
         return domainData;
     }
 
+    /**
+     * Sets the domain data.
+     *
+     * @param domainData the domain data
+     */
     public void setDomainData(List<Map<String, String>> domainData) {
         this.domainData = domainData;
     }
 
+    /**
+     * To string.
+     *
+     * @return the string
+     */
+    @Override
     public String toString() {
         ObjectMapper mapper = new ObjectMapper();
         try {
