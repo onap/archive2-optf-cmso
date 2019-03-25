5fcb0f8d7c1b5c9b6459b1e978de5c0e1370c1a2
Jerry Flood <jflood@att.com>
2019-03-25 12:22:59 -0400

    Commit 4 for  Create Optimized Sched API
    Multiple commits required due to commit size limitation.
    
    Change-Id: Ic53ce213f11b3d63f84042a6ff3e8ba065586faa
    Issue-ID: OPTFRA-458
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/main/java/org/onap/optf/cmso/model/ApprovalType.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/model/ApprovalType.java
@@ -1,39 +1,33 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * Copyright © 2017-2018 AT&T Intellectual Property. Modifications Copyright © 2018 IBM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed under the Creative
+ * Commons License, Attribution 4.0 Intl. (the "License"); you may not use this documentation except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
- * Unless otherwise specified, all documentation contained herein is licensed
- * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
- * you may not use this documentation except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         https://creativecommons.org/licenses/by/4.0/
- * 
- * Unless required by applicable law or agreed to in writing, documentation
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
+ */
 
 package org.onap.optf.cmso.model;
 
 import java.io.Serializable;
 import java.util.UUID;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -42,7 +36,7 @@ import javax.persistence.Table;
 
 /**
  * The persistent class for the approval_types database table.
- * 
+ *
  */
 @Entity
 @Table(name = "APPROVAL_TYPES")
@@ -63,39 +57,82 @@ public class ApprovalType implements Serializable {
     @Column(name = "domain")
     private String domain;
 
+    /**
+     * Instantiates a new approval type.
+     */
     public ApprovalType() {}
 
 
+    /**
+     * Gets the uuid.
+     *
+     * @return the uuid
+     */
     public UUID getUuid() {
-		return uuid;
-	}
+        return uuid;
+    }
 
 
-	public void setUuid(UUID uuid) {
-		this.uuid = uuid;
-	}
+    /**
+     * Sets the uuid.
+     *
+     * @param uuid the new uuid
+     */
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
 
 
-	public Integer getApprovalCount() {
+    /**
+     * Gets the approval count.
+     *
+     * @return the approval count
+     */
+    public Integer getApprovalCount() {
         return this.approvalCount;
     }
 
+    /**
+     * Sets the approval count.
+     *
+     * @param approvalCount the new approval count
+     */
     public void setApprovalCount(Integer approvalCount) {
         this.approvalCount = approvalCount;
     }
 
+    /**
+     * Gets the approval type.
+     *
+     * @return the approval type
+     */
     public String getApprovalType() {
         return this.approvalType;
     }
 
+    /**
+     * Sets the approval type.
+     *
+     * @param approvalType the new approval type
+     */
     public void setApprovalType(String approvalType) {
         this.approvalType = approvalType;
     }
 
+    /**
+     * Gets the domain.
+     *
+     * @return the domain
+     */
     public String getDomain() {
         return domain;
     }
 
+    /**
+     * Sets the domain.
+     *
+     * @param domain the new domain
+     */
     public void setDomain(String domain) {
         this.domain = domain;
     }
--- cmso-service/src/main/java/org/onap/optf/cmso/model/ChangeManagementChangeWindow.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/model/ChangeManagementChangeWindow.java
@@ -1,57 +1,48 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * Copyright © 2017-2018 AT&T Intellectual Property. Modifications Copyright © 2018 IBM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed under the Creative
+ * Commons License, Attribution 4.0 Intl. (the "License"); you may not use this documentation except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
- * Unless otherwise specified, all documentation contained herein is licensed
- * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
- * you may not use this documentation except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         https://creativecommons.org/licenses/by/4.0/
- * 
- * Unless required by applicable law or agreed to in writing, documentation
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
+ */
 
 package org.onap.optf.cmso.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
 import java.util.UUID;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-
 import org.joda.time.format.ISODateTimeFormat;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 
 /**
  * The persistent class for the change_management_change_windows database table.
- * 
+ *
  */
 @Entity
 @Table(name = "CHANGE_MANAGEMENT_CHANGE_WINDOWS")
@@ -86,59 +77,124 @@ public class ChangeManagementChangeWindow implements Serializable {
     @Column(name = "change_management_group_uuid")
     private UUID changeManagementGroupUuid;
 
+    /**
+     * Instantiates a new change management change window.
+     */
     public ChangeManagementChangeWindow() {}
 
 
+    /**
+     * Gets the finish time.
+     *
+     * @return the finish time
+     */
     public String getFinishTime() {
-        if (finishTimeMillis != null)
+        if (finishTimeMillis != null) {
             return ISODateTimeFormat.dateTimeNoMillis().print(this.finishTimeMillis);
+        }
         return null;
     }
 
+    /**
+     * Sets the finish time.
+     *
+     * @param finishTime the new finish time
+     */
     public void setFinishTime(String finishTime) {}
 
+    /**
+     * Gets the start time.
+     *
+     * @return the start time
+     */
     public String getStartTime() {
-        if (startTimeMillis != null)
+        if (startTimeMillis != null) {
             return ISODateTimeFormat.dateTimeNoMillis().print(this.startTimeMillis);
+        }
         return null;
     }
 
+    /**
+     * Sets the start time.
+     *
+     * @param startTime the new start time
+     */
     public void setStartTime(String startTime) {}
 
 
+    /**
+     * Gets the uuid.
+     *
+     * @return the uuid
+     */
     public UUID getUuid() {
-		return uuid;
-	}
+        return uuid;
+    }
 
 
-	public void setUuid(UUID uuid) {
-		this.uuid = uuid;
-	}
+    /**
+     * Sets the uuid.
+     *
+     * @param uuid the new uuid
+     */
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
 
 
 
-	public UUID getChangeManagementGroupUuid() {
-		return changeManagementGroupUuid;
-	}
+    /**
+     * Gets the change management group uuid.
+     *
+     * @return the change management group uuid
+     */
+    public UUID getChangeManagementGroupUuid() {
+        return changeManagementGroupUuid;
+    }
 
 
-	public void setChangeManagementGroupUuid(UUID changeManagementGroupUuid) {
-		this.changeManagementGroupUuid = changeManagementGroupUuid;
-	}
+    /**
+     * Sets the change management group uuid.
+     *
+     * @param changeManagementGroupUuid the new change management group uuid
+     */
+    public void setChangeManagementGroupUuid(UUID changeManagementGroupUuid) {
+        this.changeManagementGroupUuid = changeManagementGroupUuid;
+    }
 
 
-	public Long getFinishTimeMillis() {
+    /**
+     * Gets the finish time millis.
+     *
+     * @return the finish time millis
+     */
+    public Long getFinishTimeMillis() {
         return finishTimeMillis;
     }
 
+    /**
+     * Sets the finish time millis.
+     *
+     * @param finishTimeMillis the new finish time millis
+     */
     public void setFinishTimeMillis(Long finishTimeMillis) {
         this.finishTimeMillis = finishTimeMillis;
     }
 
+    /**
+     * Gets the start time millis.
+     *
+     * @return the start time millis
+     */
     public Long getStartTimeMillis() {
         return startTimeMillis;
     }
 
+    /**
+     * Sets the start time millis.
+     *
+     * @param startTimeMillis the new start time millis
+     */
     public void setStartTimeMillis(Long startTimeMillis) {
         this.startTimeMillis = startTimeMillis;
     }
