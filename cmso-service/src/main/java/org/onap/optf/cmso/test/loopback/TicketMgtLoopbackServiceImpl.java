683c950ac7deff02e8f4c69cafbd896a214ba23f
Jerry Flood <jflood@att.com>
2019-03-25 12:28:31 -0400

    Commit 17 for  Create Optimized Sched API
    Multiple commits required due to commit size limitation.
    
    Change-Id: I13ee156d9f4e9c5e6d55f0e0dcdef94cc3f6a1b0
    Issue-ID: OPTFRA-458
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/main/java/org/onap/optf/cmso/test/loopback/TicketMgtLoopbackServiceImpl.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/test/loopback/TicketMgtLoopbackServiceImpl.java
@@ -1,36 +1,35 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * Copyright Â© 2017-2018 AT&T Intellectual Property. Modifications Copyright Â© 2018 IBM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed under the Creative
+ * Commons License, Attribution 4.0 Intl. (the "License"); you may not use this documentation except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
- * Unless otherwise specified, all documentation contained herein is licensed
- * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
- * you may not use this documentation except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         https://creativecommons.org/licenses/by/4.0/
- * 
- * Unless required by applicable law or agreed to in writing, documentation
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
+ */
 
 package org.onap.optf.cmso.test.loopback;
 
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.HashMap;
 import java.util.Map;
 import javax.ws.rs.core.Response;
@@ -42,16 +41,10 @@ import org.onap.optf.cmso.ticketmgt.bean.TmStatusEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import com.att.eelf.configuration.EELFLogger;
-import com.att.eelf.configuration.EELFManager;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Controller
 public class TicketMgtLoopbackServiceImpl implements TicketMgtLoopbackService {
-    private static EELFLogger log = EELFManager.getInstance().getLogger(TicketMgtLoopbackServiceImpl.class);
     private static EELFLogger debug = EELFManager.getInstance().getDebugLogger();
-    private static EELFLogger errors = EELFManager.getInstance().getErrorLogger();
 
     @Autowired
     Environment env;
--- cmso-service/src/main/java/org/onap/optf/cmso/ticketmgt/TmEndpoints.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/ticketmgt/TmEndpoints.java
@@ -1,27 +1,27 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
+ * Copyright Â© 2017-2018 AT&T Intellectual Property.
+ * Modifications Copyright Â© 2018 IBM.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
+ *
+ *
  * Unless otherwise specified, all documentation contained herein is licensed
  * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
  * you may not use this documentation except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *         https://creativecommons.org/licenses/by/4.0/
- * 
+ *
  * Unless required by applicable law or agreed to in writing, documentation
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +31,8 @@
 
 package org.onap.optf.cmso.ticketmgt;
 
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -38,13 +40,10 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import com.att.eelf.configuration.EELFLogger;
-import com.att.eelf.configuration.EELFManager;
 
 @Component
 public class TmEndpoints {
     private static EELFLogger log = EELFManager.getInstance().getLogger(TmEndpoints.class);
-    private static EELFLogger errors = EELFManager.getInstance().getErrorLogger();
     private static EELFLogger debug = EELFManager.getInstance().getDebugLogger();
 
     @Autowired
@@ -72,8 +71,15 @@ public class TmEndpoints {
 
     private boolean legacyLoaded = false;
     private Map<Endpoint, List<String>> endpointMap = new HashMap<>();
-    private Map<Endpoint, String> endpointMapOK = new HashMap<>();
+    private Map<Endpoint, String> endpointMapOk = new HashMap<>();
 
+    /**
+     * Gets the endpoint.
+     *
+     * @param ep the ep
+     * @param endpoints the endpoints
+     * @return the endpoint
+     */
     public String getEndpoint(Endpoint ep, List<String> endpoints) {
         loadLegacy();
         endpoints.clear();
@@ -82,11 +88,11 @@ public class TmEndpoints {
         if (endpoints.size() > 0) {
             // Make an attempt to return the most recent "working" endpoint.
             //
-            synchronized (endpointMapOK) {
-                endpoint = endpointMapOK.get(ep);
+            synchronized (endpointMapOk) {
+                endpoint = endpointMapOk.get(ep);
                 if (endpoint == null) {
                     endpoint = endpoints.get(0);
-                    endpointMapOK.put(ep, endpoint);
+                    endpointMapOk.put(ep, endpoint);
                 }
             }
             endpoints.remove(endpoint);
@@ -96,21 +102,29 @@ public class TmEndpoints {
 
     // Call this if the previous enpoint failed to connect.
     // An attempt to track the most recent "working" endpoint.
+    /**
+     * Gets the next endpoint.
+     *
+     * @param ep the ep
+     * @param endpoints the endpoints
+     * @return the next endpoint
+     */
     public String getNextEndpoint(Endpoint ep, List<String> endpoints) {
         String endpoint = null;
         if (endpoints.size() > 0) {
             endpoint = endpoints.remove(0);
-            synchronized (endpointMapOK) {
+            synchronized (endpointMapOk) {
                 // Let's hope this one works.
-                endpointMapOK.put(ep, endpoint);
+                endpointMapOk.put(ep, endpoint);
             }
         }
         return endpoint;
     }
 
     private synchronized void loadLegacy() {
-        if (legacyLoaded)
+        if (legacyLoaded) {
             return;
+        }
         log.info("Loading legacy endpoints");
         endpointMap = new HashMap<>();
         addToEndpointMap(Endpoint.CREATE);
@@ -128,6 +142,7 @@ public class TmEndpoints {
         list.add(env.getProperty(ep.toString()));
     }
 
+    @Override
     public String toString() {
         return endpointMap.toString();
     }
