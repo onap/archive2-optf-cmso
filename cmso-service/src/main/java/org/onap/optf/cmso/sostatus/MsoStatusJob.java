eefc5da9df56ca00b4cf66e1eca981a15a6b61ca
Jerry Flood <jflood@att.com>
2019-03-25 12:28:14 -0400

    Commit 16 for  Create Optimized Sched API
    Multiple commits required due to commit size limitation.
    
    Change-Id: I7b766a7ec8036460964e8dbe85d96f049515ecd8
    Issue-ID: OPTFRA-458
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/main/java/org/onap/optf/cmso/sostatus/MsoStatusJob.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/sostatus/MsoStatusJob.java
@@ -1,38 +1,34 @@
 /*
- * Copyright © 2017-2019 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * Copyright © 2017-2019 AT&T Intellectual Property. Modifications Copyright © 2018 IBM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed under the Creative
+ * Commons License, Attribution 4.0 Intl. (the "License"); you may not use this documentation except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
- * Unless otherwise specified, all documentation contained herein is licensed
- * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
- * you may not use this documentation except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         https://creativecommons.org/licenses/by/4.0/
- * 
- * Unless required by applicable law or agreed to in writing, documentation
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
+ */
 
 package org.onap.optf.cmso.sostatus;
 
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
 import java.util.UUID;
-
 import org.onap.observations.Mdc;
 import org.onap.optf.cmso.common.LogMessages;
 import org.onap.optf.cmso.model.ChangeManagementSchedule;
@@ -44,12 +40,10 @@ import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import com.att.eelf.configuration.EELFLogger;
-import com.att.eelf.configuration.EELFManager;
 
 /**
- * This is the Quartz Job that is run to send the workflow to VID for execution
- * 
+ * This is the Quartz Job that is run to send the workflow to VID for execution.
+ *
  *
  */
 @Component
@@ -63,7 +57,7 @@ public class MsoStatusJob implements Job {
     MsoStatusClient mso;
 
     @Autowired
-    ChangeManagementScheduleDAO cmScheduleDAO;
+    ChangeManagementScheduleDAO cmScheduleDao;
 
     @Autowired
     Environment env;
@@ -79,8 +73,8 @@ public class MsoStatusJob implements Job {
         String requestId = context.getJobDetail().getJobDataMap().getString(ContextKeys.msoRequestId.toString());
         debug.debug(LogMessages.MSO_STATUS_JOB, "Entered", requestId, id.toString());
         try {
-        	UUID uuid = UUID.fromString(id);
-            ChangeManagementSchedule cmSchedule = cmScheduleDAO.findById(uuid).orElse(null);
+            UUID uuid = UUID.fromString(id);
+            ChangeManagementSchedule cmSchedule = cmScheduleDao.findById(uuid).orElse(null);
             if (cmSchedule == null) {
                 log.warn(LogMessages.MSO_POLLING_MISSING_SCHEDULE, id, requestId);
                 return;
--- cmso-service/src/main/java/org/onap/optf/cmso/test/loopback/TicketMgtLoopbackService.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/test/loopback/TicketMgtLoopbackService.java
@@ -1,36 +1,37 @@
 /*
- * Copyright © 2017-2018 AT&T Intellectual Property.
- * Modifications Copyright © 2018 IBM.
+ * Copyright ï¿½ 2017-2018 AT&T Intellectual Property. Modifications Copyright ï¿½ 2018 IBM.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ * 
+ * Unless otherwise specified, all documentation contained herein is licensed under the Creative
+ * Commons License, Attribution 4.0 Intl. (the "License"); you may not use this documentation except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * https://creativecommons.org/licenses/by/4.0/
+ * 
+ * Unless required by applicable law or agreed to in writing, documentation distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
- * 
- * 
- * Unless otherwise specified, all documentation contained herein is licensed
- * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
- * you may not use this documentation except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *         https://creativecommons.org/licenses/by/4.0/
- * 
- * Unless required by applicable law or agreed to in writing, documentation
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
+ */
 
 package org.onap.optf.cmso.test.loopback;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -38,12 +39,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import com.fasterxml.jackson.databind.JsonNode;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @Api
 @Path("/v1/tm")
@@ -56,7 +51,7 @@ public interface TicketMgtLoopbackService {
     @Produces({MediaType.APPLICATION_JSON})
     @ApiOperation(value = "", notes = "Dummy out ticket management check status call.", response = JsonNode.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
+                    @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
     public Response tmGetChangeRecord(@ApiParam(value = "TM request message") JsonNode request, @Context UriInfo uri);
 
     // ******************************************************************
@@ -65,9 +60,9 @@ public interface TicketMgtLoopbackService {
     @Produces({MediaType.APPLICATION_JSON})
     @ApiOperation(value = "", notes = "Dummy out ticket management create call.", response = JsonNode.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
+                    @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
     public Response tmCreateChangeRecord(@ApiParam(value = "TM request message") JsonNode request,
-            @Context UriInfo uri);
+                    @Context UriInfo uri);
 
     // ******************************************************************
     @POST
@@ -75,19 +70,19 @@ public interface TicketMgtLoopbackService {
     @Produces({MediaType.APPLICATION_JSON})
     @ApiOperation(value = "", notes = "Dummy out ticket management close call.", response = JsonNode.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
+                    @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
     public Response tmCloseCancelChangeRecord(@ApiParam(value = "TM request message") JsonNode request,
-            @Context UriInfo uri);
+                    @Context UriInfo uri);
 
     // ******************************************************************
     @POST
     @Path("/updateChangeRecord")
     @Produces({MediaType.APPLICATION_JSON})
     @ApiOperation(value = "", notes = "Dummy out ticket management update to in progress call.",
-            response = JsonNode.class)
+                    response = JsonNode.class)
     @ApiResponses(value = {@ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
+                    @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
     public Response tmUpdateChangeRecord(@ApiParam(value = "TM request message") JsonNode request,
-            @Context UriInfo uri);
+                    @Context UriInfo uri);
 
 }
