18d61c45cf4cc124c5e3d3917ce1eac6ef9da17b
Jerry Flood <jflood@att.com>
2019-03-25 12:26:04 -0400

    Commit 11 for  Create Optimized Sched API
    Multiple commits required due to commit size limitation.
    
    Change-Id: If009d04c7e17196c3d55e27829ec7b1ed33cfcea
    Issue-ID: OPTFRA-458
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/main/java/org/onap/optf/cmso/service/rs/CmsoOptimizerCallback.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/service/rs/CmsoOptimizerCallback.java
@@ -1,27 +1,27 @@
 /*
  * Copyright © 2017-2018 AT&T Intellectual Property.
  * Modifications Copyright © 2018 IBM.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- *
+ * 
+ * 
  * Unless otherwise specified, all documentation contained herein is licensed
  * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
  * you may not use this documentation except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *         https://creativecommons.org/licenses/by/4.0/
- *
+ * 
  * Unless required by applicable law or agreed to in writing, documentation
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,30 +43,22 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import org.onap.optf.cmso.common.CMSRequestError;
-import org.onap.optf.cmso.service.rs.models.v2.OptimizedScheduleMessage;
+import org.onap.optf.cmso.optimizer.bean.CMOptimizerResponse;
 
 @Api("CMSO Optimized Schedule API")
 @Path("/{apiVersion}")
 @Produces({MediaType.APPLICATION_JSON})
-public interface CmsoOptimizedScheduleService {
+public interface CmsoOptimizerCallback {
 
     // ******************************************************************
     @POST
-    @Path("/schedules/optimized/{scheduleId}")
+    @Path("/optimizerCallback")
     @Produces({MediaType.APPLICATION_JSON})
-    @ApiOperation(value = "", notes = "Creates a request for an optimized schedule")
-    @ApiResponses(
-            value = {@ApiResponse(code = 202, message = "Schedule request accepted for optimization."),
-                    @ApiResponse(code = 409, message = "Schedule request already exists for this schedule id.",
-                            response = CMSRequestError.class),
-                    @ApiResponse(code = 500, message = "Unexpected Runtime error")})
-    public Response createScheduleRequest(
+    @ApiOperation(value = "", notes = "Processes optimizer results callback to a Pending Optimization schedule.")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 500, message = "Unexpected Runtime error", response = Exception.class)})
+    public Response sniroCallback(
             @ApiParam(value = "v1") @PathParam("apiVersion") @DefaultValue("v1") String apiVersion,
-            @ApiParam(value = "Schedule id to uniquely identify the schedule request being created.")
-            @PathParam("scheduleId") String scheduleId,
-            @ApiParam(value = "Data for creating a schedule request for the given schedule id")
-            OptimizedScheduleMessage scheduleMessage);
-
+            @ApiParam(value = "Return schedules > lastScheduleId") CMOptimizerResponse reponse);
 
 }
