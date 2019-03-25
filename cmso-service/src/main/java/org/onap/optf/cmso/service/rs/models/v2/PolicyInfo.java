ff8eb8600bbd26106b1ddc070c8bc7bfce575a3e
Jerry Flood <jflood@att.com>
2019-03-25 12:27:52 -0400

    Commit 15 for  Create Optimized Sched API
    Multiple commits required due to commit size limitation.
    
    Change-Id: I023a8bf8516b896579a8f2bed5d503deadd04559
    Issue-ID: OPTFRA-458
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/main/java/org/onap/optf/cmso/service/rs/models/v2/PolicyInfo.java
+++ cmso-service/src/main/java/org/onap/optf/cmso/service/rs/models/v2/PolicyInfo.java
@@ -1,46 +1,41 @@
 /*******************************************************************************
- * 
- *  Copyright © 2019 AT&T Intellectual Property.
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *  
- *          http://www.apache.org/licenses/LICENSE-2.0
- *  
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  
- *  
- *  Unless otherwise specified, all documentation contained herein is licensed
- *  under the Creative Commons License, Attribution 4.0 Intl. (the "License");
- *  you may not use this documentation except in compliance with the License.
- *  You may obtain a copy of the License at
- *  
- *          https://creativecommons.org/licenses/by/4.0/
- *  
- *  Unless required by applicable law or agreed to in writing, documentation
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *
+ * Copyright © 2019 AT&T Intellectual Property.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed under the Creative
+ * Commons License, Attribution 4.0 Intl. (the "License"); you may not use this documentation except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
  ******************************************************************************/
-package org.onap.optf.cmso.service.rs.models.v2;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+package org.onap.optf.cmso.service.rs.models.v2;
 
 import com.att.eelf.configuration.EELFLogger;
 import com.att.eelf.configuration.EELFManager;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApiModel(value = "Supported Policy Information", description = "Policy Information returned from get policies API.")
 public class PolicyInfo implements Serializable {
@@ -53,24 +48,25 @@ public class PolicyInfo implements Serializable {
     @ApiModelProperty(value = "Named values to modify/override policy attributes.")
     public List<NameValue> policyModifiers = new ArrayList<>();
 
-	public String getPolicyName() {
-		return policyName;
-	}
+    public String getPolicyName() {
+        return policyName;
+    }
 
-	public void setPolicyName(String policyName) {
-		this.policyName = policyName;
-	}
+    public void setPolicyName(String policyName) {
+        this.policyName = policyName;
+    }
 
 
-	public List<NameValue> getPolicyModifiers() {
-		return policyModifiers;
-	}
+    public List<NameValue> getPolicyModifiers() {
+        return policyModifiers;
+    }
 
-	public void setPolicyModifiers(List<NameValue> policyModifiers) {
-		this.policyModifiers = policyModifiers;
-	}
+    public void setPolicyModifiers(List<NameValue> policyModifiers) {
+        this.policyModifiers = policyModifiers;
+    }
 
-	public String toString() {
+    @Override
+    public String toString() {
         ObjectMapper mapper = new ObjectMapper();
         try {
             return mapper.writeValueAsString(this);
