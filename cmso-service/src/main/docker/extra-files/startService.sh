9c23f5162c9cde34e69f0caa49ae154aeec9ec83
Jerry Flood <jflood@att.com>
2019-04-10 09:08:25 -0400

    Add java truststore to startup
    Issue-ID: OPTFRA-391
    
    Change-Id: I0a2dd62d0dcc4f8ddc714eae6a50b29f0f737b0b
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-service/src/main/docker/extra-files/startService.sh
+++ cmso-service/src/main/docker/extra-files/startService.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+AUTHENTICATION=${AUTHENTICATION:-proprietary-auth}
+JAVA_TRUSTSTORE=${JAVA_TRUSTSTORE:-etc/config/truststoreONAPall.jks}
+
 VM_ARGS="${VM_ARGS} -Dserver.local.startpath=${RESOURCES_HOME}"
+VM_ARGS="${VM_ARGS} -Djavax.net.ssl.trustStore==${JAVA_TRUSTSTORE}"
 
 echo "VM_ARGS="${VM_ARGS}
 
-java -Djava.security.egd=file:/dev/./urandom  ${VM_ARGS} -Xms256m -Xmx1024m  -jar  ./app.jar
+java -Djava.security.egd=file:/dev/./urandom  ${VM_ARGS} -Xms256m -Xmx1024m  -jar  ./app.jar --spring.profiles.active=${AUTHENTICATION}
