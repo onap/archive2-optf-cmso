d33842154cf8e359a643cdc976716d7fc0c93cec
Jerry Flood <jflood@att.com>
2019-04-19 14:11:27 -0400

    Update license to not be javadoc
    Issue-ID: OPTFRA-466
    Change-Id: Ic3b665edccd81efeaacdb9b032d3bd5fc7a8154c
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-topology/src/main/java/org/onap/optf/cmso/aaf/AafAuthorizationFilter.java
+++ cmso-topology/src/main/java/org/onap/optf/cmso/aaf/AafAuthorizationFilter.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright © 2019 AT&T Intellectual Property.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
