990a5833020c8b01f059c7e5f6405c0076eeed05
Jerry Flood <jflood@att.com>
2019-04-22 12:01:23 -0400

    Include ticket/topology jacoco
    Include optimizer validation test cases
    
    Issue-ID: OPTFRA-474
    Change-Id: I1ddd40b7148703a38b0840afd58f3fb667f2bf00
    Signed-off-by: Jerry Flood <jflood@att.com>

--- cmso-sonar/docker/integration/ete_test.sh
+++ cmso-sonar/docker/integration/ete_test.sh
@@ -12,6 +12,8 @@ done
 ### Shut down java with to give time to write the jacoco_exec file
 docker exec integration_cmso-service_1 pkill java
 docker exec integration_cmso-optimizer_1 pkill java
+docker exec integration_cmso-ticketmgt_1 pkill java
+docker exec integration_cmso-topology_1 pkill java
 sleep 10
 
 ### wait for  cmso  containers to exit and jacoco files written
@@ -20,11 +22,20 @@ while [ $? -ne 1 ]; do
   sleep 60
   docker exec integration_cmso-service_1 ls
 done
-
 docker exec integration_cmso-optimizer_1 ls
 while [ $? -ne 1 ]; do
   sleep 60
   docker exec integration_cmso-optimizer_1 ls
+done
+docker exec integration_cmso-ticketmgt_1 ls
+while [ $? -ne 1 ]; do
+  sleep 60
+  docker exec integration_cmso-ticketmgt_1 ls
+done
+docker exec integration_cmso-topology_1 ls
+while [ $? -ne 1 ]; do
+  sleep 60
+  docker exec integration_cmso-topology_1 ls
 done
 
 cat up.txt
