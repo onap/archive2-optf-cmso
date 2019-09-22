631f57a076305c6a5c69fa773214cabb5bee60da
Jerry Flood <jflood@att.com>
2019-09-22 10:05:26 -0400

    Re-factor AAF interface to omit cadi
    Issue-ID: OPTFRA-593
    Change-Id: I84c70e7e0830799d9b856f0931f8e79c27127f76
    Signed-off-by: Jerry Flood <jflood@att.com>

---
+++ cmso-ticketmgt/src/main/java/org/onap/optf/ticketmgt/aaf/AafUserRole.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2019 AT&T Intellectual Property.
+ * Modifications Copyright © 2018 IBM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.onap.optf.ticketmgt.aaf;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import org.onap.observations.Observation;
+import org.onap.optf.ticketmgt.common.LogMessages;
+
+/**
+ * The Class AafUserRole.
+ */
+public class AafUserRole {
+    private String url = "";
+    private String[] pathParts = {};
+    private String perm = "";
+    private String method = "";
+    private List<AafPerm> aafPerms = new ArrayList<>();
+
+    /**
+     * Instantiates a new aaf user role.
+     *
+     * @param url the url
+     * @param perm the perm
+     */
+    public AafUserRole(String url, String perm) {
+        this.setUrl(url);
+        this.setPerm(perm);
+        pathParts = url.split("\\/");
+
+        String[] perms = perm.split(",");
+        for (String p : perms) {
+            String[] parts = p.split(" ");
+            if (parts.length == 2) {
+                method = parts[1];
+            }
+            else {
+                method = "ALL";
+            }
+
+            String[] list = parts[0].split("\\|");
+            if (list.length == 3) {
+                AafPerm aafPerm = new AafPerm();
+                aafPerm.setAction(list[2]);
+                aafPerm.setInstance(list[1]);
+                aafPerm.setType(list[0]);
+                aafPerms.add(aafPerm);
+            }
+        }
+    }
+
+    /**
+     * Gets the url.
+     *
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Sets the url.
+     *
+     * @param url the new url
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Gets the perm.
+     *
+     * @return the perm
+     */
+    public String getPerm() {
+        return perm;
+    }
+
+    /**
+     * Sets the perm.
+     *
+     * @param perm the new perm
+     */
+    public void setPerm(String perm) {
+        this.perm = perm;
+    }
+
+    /**
+     * Gets the aaf perms.
+     *
+     * @return the aaf perms
+     */
+    public List<AafPerm> getAafPerms() {
+        return aafPerms;
+    }
+
+    /**
+     * Sets the aaf perms.
+     *
+     * @param aafPerms the new aaf perms
+     */
+    public void setAafPerms(List<AafPerm> aafPerms) {
+        this.aafPerms = aafPerms;
+    }
+
+    /**
+     * Matches.
+     *
+     * @param path the path
+     * @param matchMethod the match method
+     * @return true, if successful
+     */
+    public boolean matches(String path, String matchMethod) {
+        if (!this.method.equals("ALL") && !this.method.equals(matchMethod)) {
+            return false;
+        }
+        List<String> inNodes = new ArrayList<>();
+        List<String> matchNodes = new ArrayList<>();
+        String[] pathList = path.split("\\/");
+        for (String n : pathList) {
+            inNodes.add(n);
+        }
+        for (String n : pathParts) {
+            matchNodes.add(n);
+        }
+
+        while (!inNodes.isEmpty() && !matchNodes.isEmpty()) {
+            String inNode = inNodes.remove(0);
+            String matchNode = matchNodes.get(0);
+            if (matchNode.equals(inNode) || matchNode.equals("*")) {
+                matchNodes.remove(0);
+            } else {
+                if (!matchNode.equals("**")) {
+                    return false;
+                }
+            }
+        }
+
+        //
+        if (inNodes.isEmpty() && matchNodes.isEmpty()) {
+            return true;
+        }
+
+        // We have incoming nodes remaining, see if we can wildcard them
+        if (matchNodes.size() == 1) {
+            if (matchNodes.get(0).equals("**")) {
+                return true;
+            }
+            if (inNodes.size() == 1 && matchNodes.get(0).equals("*")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        try {
+            return new ObjectMapper().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            Observation.report(LogMessages.UNEXPECTED_EXCEPTION, e, e.toString());
+        }
+        return this.url;
+    }
+}
---
+++ cmso-ticketmgt/src/main/java/org/onap/optf/ticketmgt/aaf/AafUserRoleProperties.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2019 AT&T Intellectual Property.
+ * Modifications Copyright © 2018 IBM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * Unless otherwise specified, all documentation contained herein is licensed
+ * under the Creative Commons License, Attribution 4.0 Intl. (the "License");
+ * you may not use this documentation except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         https://creativecommons.org/licenses/by/4.0/
+ *
+ * Unless required by applicable law or agreed to in writing, documentation
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+package org.onap.optf.ticketmgt.aaf;
+
+import com.att.eelf.configuration.EELFLogger;
+import com.att.eelf.configuration.EELFManager;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.onap.observations.Observation;
+import org.onap.optf.ticketmgt.SpringProfiles;
+import org.onap.optf.ticketmgt.common.LogMessages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+/**
+ * The Class AafUserRoleProperties.
+ */
+@Component
+@Profile(SpringProfiles.AAF_AUTHENTICATION)
+public class AafUserRoleProperties {
+    private static EELFLogger debug = EELFManager.getInstance().getDebugLogger();
+
+    /** The env. */
+    @Autowired
+    Environment env;
+
+    private List<AafUserRole> list = new ArrayList<>();
+
+    /**
+     * Initialize permissions.
+     */
+    @PostConstruct
+    public void initializePermissions() {
+        String userRolePropertiesName = env.getProperty(AafProperties.aafUserRoleProperties.toString(),
+                        "opt/att/ajsc/config/AAFUserRoles.properties");
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(userRolePropertiesName));
+            for (String line : lines) {
+                line = line.trim();
+                if (!line.startsWith("#")) {
+                    String[] parts = line.split("=");
+                    if (parts.length == 2) {
+                        list.add(new AafUserRole(parts[0], env.resolvePlaceholders(parts[1])));
+                    } else {
+                        Observation.report(LogMessages.INVALID_ATTRIBUTE, line, userRolePropertiesName);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Observation.report(LogMessages.UNEXPECTED_EXCEPTION, e, e.getMessage());
+        }
+        debug.debug("AafUserRole.properties: " + list);
+    }
+
+    /**
+     * Gets the for url method.
+     *
+     * @param url the url
+     * @param method the method
+     * @return the for url method
+     */
+    public List<AafUserRole> getForUrlMethod(String url, String method) {
+        List<AafUserRole> userRoleList = new ArrayList<>();
+        for (AafUserRole aur : list) {
+            if (aur.matches(url, method)) {
+                userRoleList.add(aur);
+            }
+        }
+        return userRoleList;
+    }
+}
