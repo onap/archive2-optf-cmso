bca061ccb47a84fc71fe3663b6b0333229ce57c0
manisha07 <cmanis10@in.ibm.com>
2019-11-06 14:59:37 +0530

    Merge this if statement with the enclosing one
    Issue-ID: OPTFRA-631
    Change-Id: I8e52784dda80e50dec602ddc0035afc9c46b1a7f
    Signed-off-by: manisha07 <cmanis10@in.ibm.com>

--- cmso-topology/src/main/java/org/onap/optf/cmso/aaf/AafClientCache.java
+++ cmso-topology/src/main/java/org/onap/optf/cmso/aaf/AafClientCache.java
@@ -138,8 +138,8 @@ public class AafClientCache {
                 cacheAge = now;
             }
             permissions = cache.get(getCacheKey(auth));
-            if (permissions == null) {
-                if (!auth.get("password").equals("")) {
+            if ((permissions == null) && (!auth.get("password").equals(""))) {
+              
                     permissions = getPermissionsFromAaf(auth);
                     if (permissions != null) {
                         cache.put(getCacheKey(auth), permissions);
